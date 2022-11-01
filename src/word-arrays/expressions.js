b2d2056ceaed68a0fb272530e63a6326865f9abf
kovalevav <kovalevav@altarix.ru>
2022-11-01 13:36:00 +0100

    Words

--- src/word-arrays/expressions.js
+++ src/word-arrays/expressions.js
@@ -2,4 +2,13 @@ import { AdjectiveAdverb } from '../word.js';
 
 export const EXPRESSIONS = [
     new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    new AdjectiveAdverb('"не иметь понятия"', 'kein Rat wissen'),
+
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
+    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
 ];
