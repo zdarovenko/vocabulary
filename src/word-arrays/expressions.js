1ac8691b9f92c5003988eb962039d47976bda32d
kovalevav <kovalevav@altarix.ru>
2022-11-02 22:10:59 +0100

    Words

--- src/word-arrays/expressions.js
+++ src/word-arrays/expressions.js
@@ -3,8 +3,8 @@ import { AdjectiveAdverb } from '../word.js';
 export const EXPRESSIONS = [
     new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
     new AdjectiveAdverb('"не иметь понятия"', 'kein Rat wissen'),
+    new AdjectiveAdverb('снова и снова', 'immer wieder'),
 
-    // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
     // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
     // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
     // new AdjectiveAdverb('"развесить уши", прислушаться', 'große Ohren bekommen'),
